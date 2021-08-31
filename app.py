aa15605e28934780fec4fd780a6dcc7e074068ec
mr.adumatta@gmail.com <mr.adumatta@gmail.com>
2021-08-31 10:33:40 +0000

    working on local

--- app.py
+++ app.py
@@ -157,13 +157,18 @@ def adduser():
 
 @app.route("/ussd", methods = ['GET','POST'])
 def ussd():
-  # Read the variables sent via POST from our API   
+  
   session_id   = request.values.get("sessionId", None)
   serviceCode  = request.values.get("serviceCode", None)
   phone_number = request.values.get("phoneNumber", None)
   text         = request.values.get("text", "default")
 
-  if text      == '':
+#   session_id   = "sessionId"
+#   serviceCode  = "serviceCode"
+#   phone_number = "phoneNumber"
+#   text         = "1"
+
+  if text == '':
       # This is the first request. Note how we start the response with CON
       response  = "CON What would you want to check \n"
       response += "1. My Account \n"
